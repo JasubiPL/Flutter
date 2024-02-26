248f703d75a67a548b77f08a867080260ae794b7
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-02-26 13:35:13 -0600

    Se comienza con la sintaxis basica de Dart

---
+++ Dart Basics/02-tipos-de-datos.dart
@@ -0,0 +1,19 @@
+void main(){
+  //String
+  final String nombre = 'Tony';
+  final apellido = 'Stark'
+
+  nombre = 'Peter';
+
+  print('$nombre $apellido');
+
+  // Numeros
+
+  int empleados = 10;
+  double salario = 1856.32;
+
+  print( empleados );
+  print( salario );
+  
+
+}
